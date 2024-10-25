e42472fc9e2cd2b15bc138adaf3f1e92414a6073
jdanderson01 <74023705+jdanderson01@users.noreply.github.com>
2024-10-25 16:10:46 -0400

    Foundational code

---
+++ components/Navbar.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image"
+import Link from "next/link"
+
+const Navbar = () => {
+  return (
+    <nav className="border-2 border-red-500 flexBeetween max-container
+    padding-container relative z-30 py-5">
+        <Link href="/">
+        </Link>
+    </nav>
+  )
+}
+
+export default Navbar
